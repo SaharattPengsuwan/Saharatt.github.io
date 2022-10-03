57f9ecb65689a5622c80ffa3ac6c87715a1ccdfa
SaharattPengsuwan <s6506022420061@email.kmutnb.ac.th>
2022-10-04 02:49:49 +0700

    lecture1

--- test/test.py
+++ test/test.py
@@ -1,5 +1,83 @@
 
 
+from re import A
+
+
+print('Hello World')
+# Hello World
+
+print("Python Love Me")
+# Python Love Me
+
+
+print("I'm happy")
+# I'm happy
+
+print('He ask me "Who got the Book"')
+# He ask me "Who got the Book"
+
+
+print('\ta\na\ta\ta\n\ta')
+#        a
+# a      a       a
+#        a
+
+print('''\ta
+a\ta\ta
+\ta''')
+#        a
+# a      a       a
+#        a
+
+
+print('100.00')
+# 100
+
+print('%d' %100)
+# 100
+
+print('%d' %100.00)
+# 100
+
+print('%f' %100.00)
+# 100.000000
+
+print('%.2f' %100.00)
+# 100.00
+
+
+
+print('My age is',20)
+# My age is 20
+
+print('My age is %d I have %.2f Bth.' %(20.0,100.000))
+# My age is 20 I have 100.00 Bth.
+
+print('2 + 2 =',2+2)
+# 2 + 2 = 4
+
+print('2 + 2 = %.2f' %(2+2))
+# 2 + 2 = 4.00
+
+print('2 + 2 = '+ str(2+2))
+# 2 + 2 = 4
+
+
+print('Saharatt',"Pengsuwan")
+# Saharatt Pengsuwan
+
+print('Saharatt'+"Pengsuwan")
+# SaharattPengsuwan
+
+print('Saharatt '+"Pengsuwan")
+# Saharatt Pengsuwan
+
+
+print(0,1,2,3,4,5,6,7,8,9,)
+# 0 1 2 3 4 5 6 7 8 9
+
+print('%d %d %d %d %d %d %d %d %d %d' %(0,1,2,3,4,5,6,7,8,9))
+# 0 1 2 3 4 5 6 7 8 9
 
 
 
@@ -8,8 +86,6 @@
 
 
 
-for i in range(5):
-                                                    print('Hello World')
 
 
 
